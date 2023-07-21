2d7b1de2bc4672cbf671ab12c572c7c7ddee08de
EagleVector <kumarsubhambili3@gmail.com>
2023-07-21 12:59:11 +0530

    Custom error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorRespnose = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc        GET all bootcamps
@@ -26,7 +27,9 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if(!bootcamp) {
-      return res.status(400).json({ success: false});
+      return next(
+        new ErrorRespnose(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ 
@@ -34,8 +37,7 @@ exports.getBootcamp = async (req, res, next) => {
       data: bootcamp
     });
   } catch (err) {
-    // res.status(400).json({ success: false});
-    next(err);
+    next(new ErrorRespnose(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
 }
 
---
+++ utils/errorResponse.js
@@ -0,0 +1,8 @@
+class ErrorRespnose extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ErrorRespnose;
