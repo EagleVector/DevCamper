dbfdac43830b832dee95b763b94e4f915a16971d
EagleVector <kumarsubhambili3@gmail.com>
2023-07-21 13:52:13 +0530

    Handling validation errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -6,12 +6,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   // Mongoose bad ObjectID
   if (err.name === 'CastError') {
     const message = `Resource not found with the id of ${err.value}`;
     error = new ErrorRespnose(message, 404);
+  }
+
+  // Mongoose Duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate Field value entered';
+    error = new ErrorRespnose(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorRespnose(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
