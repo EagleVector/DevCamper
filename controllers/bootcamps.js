7f052e455380a9d488a1701108bc30a88f2ac16d
EagleVector <kumarsubhambili3@gmail.com>
2023-07-24 09:48:40 +0530

    Adding Pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,12 +14,10 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // Loop over removeFields and delete them from the reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
-
-  console.log(reqQuery);
 
   // Create query string
   let queryStr = JSON.stringify(reqQuery);
@@ -31,29 +29,56 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   query = Bootcamp.find(JSON.parse(queryStr));
 
   // Select Fields
-  if(req.query.select) {
+  if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
   }
 
   // Sort 
-  if(req.query.sort) {
+  if (req.query.sort) {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
   } else {
     query = query.sort('-createdAt');
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Executing query
   const bootcamps = await query;
 
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
   res
-  .status(200)
-  .json({
-    success: true,
-    count: bootcamps.length,
-    data: bootcamps
-  })
+    .status(200)
+    .json({
+      success: true,
+      count: bootcamps.length,
+      pagination,
+      data: bootcamps
+    })
 });
 
 // @desc        GET single bootcamp
@@ -142,10 +167,10 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
-    location: { $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] } }
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
   });
 
-  res.status(200).json ({
+  res.status(200).json({
     success: true,
     count: bootcamps.length,
     data: bootcamps
