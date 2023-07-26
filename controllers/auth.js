6bd97c224c1dc4a8d24bcac1cf917bc04b878f43
EagleVector <kumarsubhambili3@gmail.com>
2023-07-26 15:22:28 +0530

    Authentication setup

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const ErrorRespnose = require('../utils/errorResponse');
+const asyncHandler = require('../middlewares/async');
+const User = require('../models/User');
+
+// @desc        Register user
+// @route       POST /api/v1/auth/register
+// @access      Public
+
+exports.register = asyncHandler(async (req, res, next) => {
+  res.status(200).json({ success: true });
+})
