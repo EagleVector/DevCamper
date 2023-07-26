cc8b6200ccaf8ebf63dd40238e3e30f4c6e2098e
EagleVector <kumarsubhambili3@gmail.com>
2023-07-26 20:41:38 +0530

    Protecting the routes for post, update and delete using the auth bearer token

--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,8 @@ const {
 const Course = require('../models/Course');
 const advancedResults = require('../middlewares/advancedResults');
 
+const { protect } = require('../middlewares/auth');
+
 const router = express.Router({ mergeParams: true });
 
 router
@@ -19,12 +21,12 @@ router
     path: 'bootcamp',
     select: 'name description'
   }), getCourses)
-  .post(addCourse);
+  .post(protect, addCourse);
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
